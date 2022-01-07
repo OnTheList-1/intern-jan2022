60765f979e11ca127bb20c75c9a278cdfc4010cb
OnTheList-1 <66695960+OnTheList-1@users.noreply.github.com>
2022-01-07 13:54:22 +0700

    fix bugs

--- bai3/bai65.h
+++ bai3/bai65.h
@@ -5,7 +5,13 @@
 void sn65(int a, int b, int c)
 {
     double delta = (b * b) - (4 * a * c);
-    if (delta > 0)
+    if (a == 0)
+    {
+        double x1 = -c / b;
+        std::cout << "x1 = " << x1 << std::endl;
+
+    }
+    else if (delta > 0)
     {
         double x1 = (-b + sqrt(delta)) / (2 * a);
         double x2 = (-b - sqrt(delta)) / (2 * a);
