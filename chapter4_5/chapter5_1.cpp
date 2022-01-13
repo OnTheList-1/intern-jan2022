775eac5582a9fd85788d990fed69d66d71e382ad
OnTheList-1 <66695960+OnTheList-1@users.noreply.github.com>
2022-01-13 14:50:01 +0700

    fixed bugs

--- chapter4_5/chapter5_1.cpp
+++ chapter4_5/chapter5_1.cpp
@@ -5,6 +5,8 @@
 #include <array>
 #include <map>
 #include <iostream>
+#include <unordered_set>
+
 
 #include "chapter5_1.h"
 #include "chapter5.h"
@@ -34,7 +36,7 @@ bool isOdd(const std::string& fString)
 }
 
 template <typename T>
-void printArray(T* array, int size)
+void printArray(T* array, const int& size)
 {
     for (int i = 0; i < size; ++i)
     {
@@ -61,50 +63,49 @@ void swap(T& a, T& b)
 float roundFloat(float& a)
 {
     int integer = (int)a;
-    float getDecimal = a - (float)integer;
-    if (getDecimal < 0.5) {
-        return std::floor(getDecimal + integer);
+    float decimal = a - (float)integer;
+    if (decimal < 0.5) {
+        return std::floor(a);
     }
 
-    return std::ceil(getDecimal + integer);
+    return std::ceil(a);
 }
 
 template <typename T>
-bool checkAscending(T* array, const int& size)
+bool isAscending(T* array, const int& size)
 {
     for (int i = 0; i < size - 1; ++i)
     {
         if (array[i] > array[i + 1])
-            return true;
+            return false;
     }
-    return false;
+    return true;
 }
 
-int countAinB(const std::vector<float>& array1, const std::vector<float>& array2)
+int countAinB(const std::vector<float>& array1, const std::vector<float>& array2) // needs fix
 {
-    if (array1.size() > array2.size())
-        return 0;
-
-    std::map<float, int> m;
-    int countMin = -1;
-
+    int count = 0;
     for (int i = 0; i < array2.size(); ++i)
     {
-        for (int j = 0; j < array1.size(); ++j)
+        if (array1[0] == array2[i] && array2.size() - i >= array1.size())
         {
-            if (array1[j] == array2[i])
-                m[array1[j]]++;
+            int start = i;
+            bool flag = true;
+            for (int j = 0; j < array1.size(); ++j)
+            {
+                if (array1[j] != array2[start])
+                {
+                    flag = false;
+                    break;
+                }
+                ++start;
+            }
+            if (flag == true)
+                ++count;
+
         }
     }
-
-    for (const auto& itr : m)
-    {
-        if (countMin < 0)
-            countMin = itr.second;
-        countMin = std::min(countMin, itr.second);
-    }
-
-    return countMin;
+    return count;
 }
 
 void listModeInArray(const std::vector<int>& array)
@@ -172,11 +173,11 @@ void sortArrayOddAscending(std::vector<float>& array)
 
     for (int i = 0; i < array.size(); ++i)
     {
-        if (isOdd(std::to_string(array[i])) == true)
+        if (i % 2 == 1)
         {
-            for (int j = i + 1; j < array.size(); ++j)
+            for (int j = i + 2; j < array.size(); ++j)
             {
-                if (array[i] > array[j] && isOdd(std::to_string(array[j])) == true)
+                if (array[i] > array[j])
                 {
                     float temp = array[i];
                     array[i] = array[j];
@@ -277,28 +278,42 @@ void sortWhileInsert(float* array, const int& size)
 
 void deleteAllK(float* array, int& size, const float& k)
 {
-    for (int i = 0; i < size; ++i)
+
+    int i = 0;
+    while (i < size)
     {
+
         if (array[i] == k)
         {
             for (int j = i; j < size - 1; ++j)
-                array[j] = array[j + 1]; //shift left
+                array[j] = array[j + 1]; //shift down
+
             --size;
+
         }
+        else
+        {
+            ++i;
+        }
+
     }
 
     printArray(array, size);
 }
 
-void deleteEvenNumbers(int* array, int& size)
+void deleteEvenNumbers(int* array, int& size) //needs fix
 {
-    for (int i = 0; i < size; ++i)
+    int i = 0;
+    while (i < size)
     {
         if (array[i] % 2 == 0)
         {
             for (int j = i; j < size - 1; ++j)
                 array[j] = array[j + 1];
             --size;
+        }
+        else {
+            ++i;
         }
     }
 
@@ -307,34 +322,55 @@ void deleteEvenNumbers(int* array, int& size)
 
 void deleteDuplicates(int* array, int& size)
 {
-    std::map<int, int> m;
-    for (int i = 0; i < size; ++i) // unique elements in map
-        m[array[i]]++;
+    std::unordered_set<int> s;
+    for (int i = 0; i < size; ++i) // unique elements in set
+        s.insert(array[i]);
 
     int index = 0;
-    for (const auto& itr : m) {
-        array[index] = itr.first;
+    for (const auto& itr : s) {
+        array[index] = itr;
         ++index;
     }
 
-    size = (int)m.size();
+    size = (int)s.size();
 
     printArray(array, size);
 };
 
-void sortBai281(int* array, const int& size)
+void sortBai281(int* array, const int& size) // needs fix
 {
-    for (int i = 0; i < size / 2; ++i)
+    int lastPositiveIndex = 0;
+    for (int i = 0; i < size; ++i) // move even number to start of the array
     {
-        for (int j = (size - 1) / 2; j >= 0; --j)
+        if (array[i] % 2 == 0 && array[i] != 0)
         {
-            if (array[j] % 2 == 0 && array[i] % 2 == 1 && array[j] != 0)
-                swap(array[i], array[j]);
+            int temp = array[i];
+            for (int j = i; j > 0; --j)
+                array[j] = array[j - 1];
 
+            array[0] = temp;
         }
     }
 
-    //deal with zero
+    for (int i = 0; i < size; ++i) //find last positive index
+    {
+        if (array[i] % 2 != 0)
+        {
+            lastPositiveIndex = i;
+            break;
+        }
+    }
+
+    for (int i = 0; i < size; i++) //move 0 to last positive number's index
+    {
+        if (array[i] == 0)
+        {
+            for (int j = i; j > lastPositiveIndex; --j)
+                array[j] = array[j - 1];
+
+            array[lastPositiveIndex] = 0;
+        }
+    }
 
     printArray(array, size);
 }
@@ -363,7 +399,7 @@ void reverseEvenPosition(int* array, const int& size)
 void rotateArray(int* array, const int& size)
 {
     int first = array[0];
-    for (int i = 0; i < size; ++i)
+    for (int i = 0; i < size - 1; ++i)
     {
         array[i] = array[i + 1];
     }
@@ -380,7 +416,7 @@ void rotateArrayKTimes(int* array, const int& size, const int& k)
         position.push_back(array[i]); //fix
     }
 
-    for (int i = 0; i < size; ++i)
+    for (int i = 0; i < size - k; ++i)
     {
         array[i] = array[i + k];
     }
@@ -448,8 +484,25 @@ void listSubsetAscending(int* array, const int& size)
                 ascendingArray[temp] = array[k];
                 ++temp;
             }
-            if (checkAscending(ascendingArray, temp) == false && temp > 1)
+            if (isAscending(ascendingArray, temp) == false && temp > 1)
                 printArray(ascendingArray, temp);
+        }
+    }
+}
+
+void subsetSumM(int* array, const int& size, int* subset, const int& subsetSize, const int& total, const int& nodeCount, const int& sum)
+{
+    if (total == sum)
+    {
+        printArray(subset, subsetSize);
+        return;
+    }
+    else
+    {
+        for (int i = nodeCount; i < size; ++i)
+        {
+            subset[subsetSize] = array[size]; //initialize position
+            subsetSumM(array, size, subset, subsetSize + 1, total + array[i], i + 1, sum);
         }
     }
 }
--- chapter4_5/main.cpp
+++ chapter4_5/main.cpp
@@ -3,12 +3,63 @@
 #include "chapter5_1.h"
 #include <map>
 
+template <typename T> //bai 301 -- wrong needs fix
+bool isSubset(const T* array1, const T& size1, const T* array2, const T& size2)
+{
+    if (size1 > size2)
+    {
+        return false;
+    }
+
+    bool isAnElement = false;
+
+    for (int i = 0; i < size1; ++i)
+    {
+        for (int j = 0; j < size2; ++j)
+        {
+            if (array1[j] != array2[i])
+                isAnElement = true;
+        }
+        if (isAnElement)
+            return false;
+    }
+    return true;
+}
+
+template <typename T> //bai 302
+int countAinB(const T* array1, const T& size1, const T* array2, const T& size2)
+{
+    int count = 0;
+
+    for (int i = 0; i < size1; ++i)
+    {
+        if (array1[0] == array2[i] && size2 - i >= size1)
+        {
+            int startPosition = i;
+            bool checkSubset = true;
+            for (int j = 0; j < size1; j++)
+            {
+                if (array1[j] != array2[startPosition])
+                {
+                    checkSubset = 0;
+                    break;
+                }
+                ++startPosition;
+            }
+            if (checkSubset)
+                ++count;
+
+        }
+    }
+    return count;
+}
+
 int main() {
     std::cout << "---------- Chapter 5 ----------" << "\n\n";
     std::vector<int> intType;
     std::vector<float> floatType;
-    std::cout << "Enter a vector containing int datatype: \n\n";
-    insertVector(intType);
+    //std::cout << "Enter a vector containing int datatype: \n\n";
+    //insertVector(intType);
     std::cout << "\nEnter a vector containing float datatype: \n\n";
     insertVector(floatType);
 
@@ -77,87 +128,90 @@ int main() {
     std::cout << "\nBai 236: Count the number of times A appears in B.\n";
     std::vector<float> floatTypeExample{ 1, 2 };
     std::cout << "Answer: " << countAinB(floatTypeExample, floatType) << std::endl;
-    std::cout << "\nBai 238: List the mode in the array.\n";
-    listModeInArray(intType);
-    std::cout << "\n\nBai 241: Consecutive Zeros\n";
-    std::cout << "Answer: " << isConsecutiveZeros(intType) << "\n";
-    std::cout << "\nBai 246: Symetrical Array\n";
-    std::cout << "Answer : " << isSymetricalArray(floatType) << "\n";
-    std::cout << "\nBai 249: Descending Array\n";
-    std::cout << "Answer: " << isDescendingArray(floatType) << "\n";
-    std::cout << "\nBai 256: Sort Array Descending\n";
-    std::vector<float> copy256(floatType.begin(), floatType.end());
-    sortArrayDescending(copy256);
-    std::cout << "\n\nBai257: Sort Array Odd elements ascending.\n";
-    std::vector<float> copy257(floatType.begin(), floatType.end());
-    sortArrayOddAscending(copy257);
-    std::cout << "\nBai261: Sort array in ascending positive numbers.\n";
-    std::vector<float> copy261(floatType.begin(), floatType.end());
-    sortArrayPositiveAscending(copy261);
-    std::cout << "\nBai 263: Sort array where positive ascends and negative descends\n";
-    std::vector<float> copy263(floatType.begin(), floatType.end());
-    sortArrayBai263(copy263);
-    std::cout << "\nBai 266: Insert element into an array.";
-    int copy266[100] = { 1,2,3,4,5 };
-    insertAtK(copy266, 5, 2);
-    std::cout << "\n Bai 267: Sort while insert\n";
-    float copy267[100];
-    sortWhileInsert(copy267, 5);
-    std::cout << "\nBai 271: Delete all element K in the array\n";
-    float copy271[100] = { 1, 2, 3, 4, 5, 1, 3 };
-    int sz271 = 7;
-    deleteAllK(copy271, sz271, 1);
-    std::cout << "\nBai 274: Delete all even element K in the array\n";
-    int copy274[100] = { 1, 2, 3, 4, 5, 6 };
-    int sz274 = 6;
-    deleteEvenNumbers(copy274, sz274);
-    std::cout << "\nBai 279: Delete Duplicates\n";
-    int copy279[100] = { 3, 1, 1, 2, 0, 8, 3, 7, 2 };
-    int size279 = 9;
-    deleteDuplicates(copy279, size279);
+    //std::cout << "\nBai 238: List the mode in the array.\n";
+    //listModeInArray(intType);
+    //std::cout << "\n\nBai 241: Consecutive Zeros\n";
+    //std::cout << "Answer: " << isConsecutiveZeros(intType) << "\n";
+    //std::cout << "\nBai 246: Symetrical Array\n";
+    //std::cout << "Answer : " << isSymetricalArray(floatType) << "\n";
+    //std::cout << "\nBai 249: Descending Array\n";
+    //std::cout << "Answer: " << isDescendingArray(floatType) << "\n";
+    //std::cout << "\nBai 256: Sort Array Descending\n";
+    //std::vector<float> copy256(floatType.begin(), floatType.end());
+    //sortArrayDescending(copy256);
+    //std::cout << "\n\nBai257: Sort Array Odd elements ascending.\n";
+    //std::vector<float> copy257(floatType.begin(), floatType.end());
+    //sortArrayOddAscending(copy257);
+    //std::cout << "\nBai261: Sort array in ascending positive numbers.\n";
+    //std::vector<float> copy261(floatType.begin(), floatType.end());
+    //sortArrayPositiveAscending(copy261);
+    //std::cout << "\nBai 263: Sort array where positive ascends and negative descends\n";
+    //std::vector<float> copy263(floatType.begin(), floatType.end());
+    //sortArrayBai263(copy263);
+    //std::cout << "\nBai 266: Insert element into an array.";
+    //int copy266[100] = { 1,2,3,4,5 };
+    //insertAtK(copy266, 5, 2);
+    //std::cout << "\n Bai 267: Sort while insert\n";
+    //float copy267[100];
+    //sortWhileInsert(copy267, 5);
+    //std::cout << "\nBai 271: Delete all element K in the array\n";
+    //float copy271[100] = { 1, 1, 1, 1, 2, 3, 4, 5, 1, 3, 7 };
+    //int sz271 = 11;
+    //deleteAllK(copy271, sz271, 1);
+    //std::cout << "\nBai 274: Delete all even element K in the array\n";
+    //int copy274[100] = { 7, 8, 7, 1, 2, 3, 4, 5, 6 };
+    //int sz274 = 9;
+    //deleteEvenNumbers(copy274, sz274);
+    //std::cout << "\nBai 279: Delete Duplicates\n";
+    //int copy279[100] = { 3, 1, 1, 2, 0, 8, 3, 7, 2 };
+    //int size279 = 9;
+    //deleteDuplicates(copy279, size279);
+    //std::cout << "\nBai 281: Sort array where even goes front, 0 mid and odd back\n";
+    //int copy281[100] = { 4, 1, 8, 0, -3, 2, -1, 0, 1 };
+    //int size281 = 8;
+    //sortBai281(copy281, size281);
+    //std::cout << "\nBai 283: Reverse Array\n";
+    //int copy283[100] = { 1, 2, 3, 4, 5 };
+    //int size283 = 4;
+    //reverseArray(copy283, size283);
+    //std::cout << "\nBai 284: Reverse Even number's position.\n";
+    //int copy284[100] = { 1, 5, 3, 2, 1, 8, 7 };
+    //int size284 = 7;
+    //reverseEvenPosition(copy284, size284);
+    //std::cout << "\nBai 286: Rotote Array anti clockwise\n";
+    //int copy286[100] = { 1, 3, 2, 6, 5 };
+    //int size286 = 5;
+    //rotateArray(copy286, size286);
+    //std::cout << "\nBai 287: Rotate Array K Times to the right\n";
+    //int copy287[100] = { 1, 3, 2, 6, 7, 8, 3, 0 };
+    //int size287 = 8;
+    //rotateArrayKTimes(copy287, size287, 3);
+    //std::cout << "\nBai 289: Print odd and even separated by a line break.\n";
+    //printBai289(copy287, size287);
+    //std::cout << "\nBai 292: Promote array to int.\n";
+    //float bai292[100] = { 1.2f, 3, 4.5f, 5.4f, 2, 5.1f, 0 };
+    //int sizebai292 = 7;
+    //arrayToInt(bai292, sizebai292);
+    //std::cout << "\nBai 293: List all subset in an array.\n";
+    //int bai293[100] = { 1, 2, 3 };
+    //int size293 = 3;
+    //listArraySubset(bai293, size293);
+    //std::cout << "\nBai 295: List ascending subset in an array.\n";
+    //int bai295[100] = { 1, 2, 3, 1, 5, 2, 1, 5, 4, 5 };
+    //int size295 = 10;
+    //listSubsetAscending(bai295, size295);
 
-    std::cout << "\nBai 281: Sort array where even goes front, 0 mid and odd back\n";
-    int copy291[100] = { 1, 8, -3, 2, -1, 0, 1 };
-    int size281 = 7;
-    sortBai281(copy291, size281);
+    /*std::cout << "Bai 301: is A a subset of B.\n";
+    int a[100] = { 1 };
+    int b[100] = { 1, 2 , 3 ,4, 5 };
+    std::cout << "Answer: " << isSubset(a, 2, b, 5) << "\n";
 
-    std::cout << "\nBai 283: Reverse Array\n";
-    int copy283[100] = { 1, 2, 3, 4, 5 };
-    int size283 = 4;
-    reverseArray(copy283, size283);
+    std::cout << "\nBai 302: count A in B.\n";
+    std::cout << countAinB(a, 1, b, 5);*/
 
-    std::cout << "\nBai 284: Reverse Even number's position.\n";
-    int copy284[100] = { 1, 5, 3, 2, 1, 8, 7 };
-    int size284 = 7;
-    reverseEvenPosition(copy284, size284);
-
-    std::cout << "\nBai 286: Rotote Array anti clockwise\n";
-    int copy286[100] = { 1, 3, 2, 6, 5 };
-    int size286 = 5;
-    rotateArray(copy286, size286);
-
-    std::cout << "\nBai 287: Rotate Array K Times to the right\n";
-    int copy287[100] = { 1, 3, 2, 6, 7, 8, 3, 0 };
-    int size287 = 8;
-    rotateArrayKTimes(copy287, size287, 3);
-
-    std::cout << "\nBai 289: Print odd and even separated by a line break.\n";
-    printBai289(copy287, size287);
-
-    std::cout << "\nBai 292: Promote array to int.\n";
-    float bai292[100] = { 1.2f, 3, 4.5f, 5.4f, 2, 5.1f, 0 };
-    int sizebai292 = 7;
-    arrayToInt(bai292, sizebai292);
-
-    std::cout << "\nBai 293: List all subset in the array.\n";
-    int bai293[100] = { 1, 2, 3 };
-    int size293 = 3;
-    listArraySubset(bai293, size293);
-
-    std::cout << "\nBai 295: List ascending subset in array.\n";
-    int bai295[100] = { 1, 2, 3, 1, 5, 2, 1, 5, 4, 5 };
-    int size295 = 10;
-    listSubsetAscending(bai295, size295);
-
+    std::cout << "\nBai 304: Subset in A that sum up to M.\n";
+    int bai304[100] = { 3, 6, 2, 9, 11, 33, 2, 5, 1, 4, 9 };
+    int subset[100];
+    subsetSumM(bai304, 11, subset, 0, 0, 0, 11);
     return 0;
 }
