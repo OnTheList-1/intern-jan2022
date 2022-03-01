a0e34b90827ced0e31db71fc09eb11f263392722
OnTheList-1 <66695960+OnTheList-1@users.noreply.github.com>
2022-03-01 18:30:55 +0700

    added bit manipulation

--- int128_t/main.cpp
+++ int128_t/main.cpp
@@ -3,5 +3,32 @@
 int main()
 {
 	int128_t test;
-	test.ReadConsoleString();
+	int userInput = 0;
+	bool flag = true;
+	std::string strInput;
+
+	while (flag)
+	{
+		std::cout << "1. Enter Expression.\n";
+		std::cout << "2. Enter Decimal.\n";
+		std::cout << "5. QUIT\n\n";
+		std::cin >> userInput;
+
+		switch (userInput)
+		{
+		case 1:
+			std::cout << "Enter the expression: ";
+			std::cin >> strInput;
+			std::cout << test.GetPolishNotation(strInput) << "\n";
+			break;
+		case 2:
+			test.ReadConsoleString();
+			break;
+		case 5:
+			flag = false;
+			break;
+		default:
+			break;
+		}
+	}
 }
