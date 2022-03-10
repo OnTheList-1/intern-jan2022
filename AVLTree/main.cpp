4a97b7d66a3749fa5d8b94bd7d48219e51dae79f
OnTheList-1 <66695960+OnTheList-1@users.noreply.github.com>
2022-03-10 18:04:56 +0700

    misc

--- AVLTree/main.cpp
+++ AVLTree/main.cpp
@@ -5,13 +5,6 @@
 int main()
 {
 	AVLTree tree;
-	tree.Insert(10);
-	tree.Insert(20);
-	tree.Insert(30);
-	tree.Insert(40);
-	tree.Insert(50);
-	tree.Insert(25);
-	tree.Insert(48);
 
 	tree.VisualizeTree();
 
